02db56cf12eb05fbd37e0fc4bf90dace3cf72407
F-Pawamatra <72060401+F-Pawamatra@users.noreply.github.com>
2021-06-09 12:12:45 +0700

    First Commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+@extends('layout/main')
+
+@section('title', "About me")
+
+@section('active-about', "active")
+
+@section('container')
+  <div class="container">
+    <div class="row">
+        <div class="col-10">
+          <h1 class="mt-3">Hello, {{$nama}}!</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layout/main')
+
+@section('title', "Fery's blog")
+
+@section('active-home', "active")
+
+@section('container')
+  <div class="container">
+    <div class="row">
+        <div class="col-10">
+          <h1 class="mt-3">Hello, world!</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
